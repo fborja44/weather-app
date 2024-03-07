c9941ad7dfb14115338d88deb9762e38ed847380
Francis Borja <fborja4400@gmail.com>
2024-03-07 17:52:56 -0500

    fix: app stuck on splash screen

--- app/_layout.js
+++ app/_layout.js
@@ -1,45 +1,51 @@
 import { Stack } from 'expo-router';
-import { useCallback, useEffect } from 'react';
-import { useFonts } from 'expo-font';
+import { useEffect, useState } from 'react';
+import * as Font from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import Footer from '../components/footer/Footer';
 import { Provider as ReduxProvider } from 'react-redux';
 import { store, persistor } from '../redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { StatusBar } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-SplashScreen.preventAutoHideAsync(); // Show splash screen while app is loading
+SplashScreen.preventAutoHideAsync()
+	.then((result) =>
+		console.log(`SplashScreen.preventAsync() succeeded: ${result}`)
+	)
+	.catch(console.warn); // Show splash screen while app is loading
 
 const Layout = () => {
-	const [fontsLoaded, fontError] = useFonts({
-		Inter: require('../assets/fonts/Inter.ttf'),
-		Questrial: require('../assets/fonts/Questrial.ttf'),
-	});
+	const [appIsReady, setAppIsReady] = useState(false);
 
 	useEffect(() => {
-		if (fontError) throw fontError;
-	}, [fontError]);
+		const prepare = async () => {
+			try {
+				setAppIsReady(false);
+				await Font.loadAsync({
+					Inter: require('../assets/fonts/Inter.ttf'),
+					Questrial: require('../assets/fonts/Questrial.ttf'),
+				});
+			} catch (err) {
+				console.warn(err);
+			} finally {
+				setAppIsReady(true);
+        await SplashScreen.hideAsync(); // Hide splash screen when finished loading
+			}
+		};
+		prepare();
+	}, []);
 
-	const onLayoutRootView = useCallback(async () => {
-		if (fontsLoaded) {
-			await SplashScreen.hideAsync(); // Hide splash screen when finished loading
-		}
-	}, [fontsLoaded]);
-
-	if (!fontsLoaded && !fontError) {
+	if (!appIsReady) {
 		return null;
 	}
 
 	return (
 		<ReduxProvider store={store}>
-			<SafeAreaProvider>
-				<PersistGate loading={null} persistor={persistor}>
-					<StatusBar barStyle='light-content' />
-					<Stack onLayout={onLayoutRootView} />
-					<Footer />
-				</PersistGate>
-			</SafeAreaProvider>
+			<PersistGate loading={null} persistor={persistor}>
+				<StatusBar barStyle='light-content' />
+				<Stack />
+				<Footer />
+			</PersistGate>
 		</ReduxProvider>
 	);
 };
